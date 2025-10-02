432f9db2915b2dcde7b83db60ca09e115ea8d3d4
Ra_Rafiq <119992536+RofiqulIsalm@users.noreply.github.com>
2025-10-02 12:25:49 +0600

    Updated PrivacyPolicy.tsx

--- src/pages/PrivacyPolicy.tsx
+++ src/pages/PrivacyPolicy.tsx
@@ -135,10 +135,7 @@ const PrivacyPolicy: React.FC = () => {
           </h2>
           <div className="text-slate-300">
             <p className="text-slate-300">
-            If you have any questions about this Privacy Policy or our data practices,                     please{' '}
-            <a href="/contact-us" className="text-blue-400 hover:text-blue-300 underline">
-              contact us
-            </a>{' '}
+            If you have any questions about this Privacy Policy or our data practices,                     please<Link to="/contact-us" className="text-slate-300 hover:text-blue-400 transition-colors">Contact Us</Link>
             through our Contact Us page or reach out to us directly.
           </p>
           </div>
