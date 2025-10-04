117cf6654c81d5471c0a65a1e3864169ffaff7ce
Ra_Rafiq <119992536+RofiqulIsalm@users.noreply.github.com>
2025-10-04 22:07:13 +0600

    Updated TextCounter.tsx

--- src/pages/TextCounter.tsx
+++ src/pages/TextCounter.tsx
@@ -181,7 +181,7 @@ const TextToolsPage: React.FC = () => {
           </button>
         
           <button
-            className={`px-4 py-2 rounded-xl font-semibold ${selectedTab === 'binarytotext' ? 'bg-green-600 text-white' : 'bg-slate-700 text-slate-300'}`}
+            className={`px-4 py-2 rounded-xl font-semibold ${selectedTab === 'binarytotext' ? 'bg-violet-600 text-white' : 'bg-slate-700 text-slate-300'}`}
             onClick={() => setSelectedTab('binarytotext')}
           >
             Binary ↔ Text 
