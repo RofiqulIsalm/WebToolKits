331afd0e700743da8e9e14964cac5b2af272a065
Ra_Rafiq <119992536+RofiqulIsalm@users.noreply.github.com>
2025-10-05 10:09:23 +0600

    Updated TextCounter.tsx

--- src/pages/TextCounter.tsx
+++ src/pages/TextCounter.tsx
@@ -281,6 +281,14 @@ const dropdownRef = useRef<HTMLDivElement>(null);
           >
             Binary ↔ Text 
           </button>
+
+          <button
+            className={`px-4 py-2 rounded-xl font-semibold ${selectedTab === 'numberConverter' ? 'bg-yellow-600 text-white' : 'bg-slate-700 text-slate-300'}`}
+            onClick={() => setSelectedTab('numberConverter')}
+          >
+            Number ↔ Words
+         </button>
+
         </div>
 
         {/* ----------------- Text Counter ----------------- */}
