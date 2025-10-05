cbddf5075a7bee3686f061ba57d9fe125a23d2ba
Ra_Rafiq <119992536+RofiqulIsalm@users.noreply.github.com>
2025-10-05 15:12:09 +0600

    Updated QRCodeGenerator.tsx

--- src/pages/QRCodeGenerator.tsx
+++ src/pages/QRCodeGenerator.tsx
@@ -662,7 +662,7 @@ ${500 + imgData.length}
                         type="color"
                         value={fgColor}
                         onChange={(e) => setFgColor(e.target.value)}
-                        className="h-10 w-20 rounded border border-slate-600 cursor-pointer"
+                        className="h-10 w-20 rounded-xl  border border-slate-600 cursor-pointer"
                       />
                       <input
                         type="text"
