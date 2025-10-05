3098f7aebd407f41a59b03a7386353757cf4b02e
Ra_Rafiq <119992536+RofiqulIsalm@users.noreply.github.com>
2025-10-05 10:01:57 +0600

    Updated TextCounter.tsx

--- src/pages/TextCounter.tsx
+++ src/pages/TextCounter.tsx
@@ -133,10 +133,7 @@ const dropdownRef = useRef<HTMLDivElement>(null);
 
     setStats({ characters, charactersNoSpaces, words, sentences, paragraphs, lines,               readingTime });
 
-    const palindromeWords = wordsArray.filter(word => {
-      const cleanWord = word.replace(/[^a-z0-9]/gi, '').toLowerCase();
-      return cleanWord.length > 1 && cleanWord === cleanWord.split('').reverse().join('');
-      }).length;
+    
   };    
     
 
