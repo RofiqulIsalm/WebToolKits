bd0ffcce5694e8a3d7e2fc80ae04a73e0e7132b0
Ra_Rafiq <119992536+RofiqulIsalm@users.noreply.github.com>
2025-10-13 10:02:27 +0600

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -29,7 +29,7 @@ import AddSubtractDays from './pages/AddSubtractDays';
 import WeekdayFinder from './pages/WeekdayFinder';
 import BusinessDaysCalculator from './pages/BusinessDaysCalculator';
 import CountdownTimer from './pages/CountdownTimer';
-import TimeZoneConverter from './pages/TimeZoneConverter';
+
 
 
  
@@ -98,7 +98,7 @@ function App() {
             <Route path="/weekday-finder" element={<WeekdayFinder />} />
             <Route path="/business-days-calculator" element={<BusinessDaysCalculator />} />
             <Route path="/countdown-timer" element={<CountdownTimer />} />
-            <Route path="/time-zone-converter" element={<TimeZoneConverter />} />
+ 
             
 
             
