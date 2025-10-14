68ed4e9aa9d4246a71d8be5b07b5d507aaf7eaec
Ra_Rafiq <119992536+RofiqulIsalm@users.noreply.github.com>
2025-10-14 10:46:10 +0600

    Updated LoanEMICalculator.tsx

--- src/pages/LoanEMICalculator.tsx
+++ src/pages/LoanEMICalculator.tsx
@@ -83,7 +83,7 @@ const LoanEMICalculator: React.FC = () => {
                   onClick={() => { setPrincipal(100000); setRate(10); setTenure(12); }}
                   className="mt-4 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-full text-sm p-2.5 text-center inline-flex items-center me-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                 >
-                  Reset
+                  0
                 </button>
             </div>
           
