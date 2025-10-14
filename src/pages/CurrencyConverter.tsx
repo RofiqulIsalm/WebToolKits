ff691dbd921405802c4af7f584357af77c9c56c9
Ra_Rafiq <119992536+RofiqulIsalm@users.noreply.github.com>
2025-10-14 10:25:01 +0600

    Updated CurrencyConverter.tsx

--- src/pages/CurrencyConverter.tsx
+++ src/pages/CurrencyConverter.tsx
@@ -438,7 +438,7 @@ In short, the logic may be simple, but the impact is powerful: a <strong>currenc
             <a href="/loan-affordability-calculator" className="text-blue-400 hover:underline">Loan Affordability Calculator</a>, 
             <a href="/roi-calculator" className="text-blue-400 hover:underline">ROI Calculator</a>, 
             or 
-            <a href="/profit-margin-calculator" className="text-blue-400 hover:underline">Profit Margin Calculator</a>.
+            <a href="/profit-margin-calculator" className="text-blue-400 hover:underline"> Profit Margin Calculator</a>.
           </p>
 
       
