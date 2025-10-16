bdf771feadcf1aed4fffb1acec86b2ec7dd82209
Ra_Rafiq <119992536+RofiqulIsalm@users.noreply.github.com>
2025-10-17 01:46:24 +0600

    Updated TaxCalculator.tsx

--- src/pages/TaxCalculator.tsx
+++ src/pages/TaxCalculator.tsx
@@ -338,7 +338,7 @@ const TaxCalculator: React.FC = () => {
     
         {/* Text beside icon */}
         <div className="ml-12 w-full">
-          <p className="text-base font-medium leading-snug text-gray-500">
+          <p className="text-base font-medium leading-snug text-gray-100">
             {tipsForCountry[activeTip]}
           </p>
         </div>
