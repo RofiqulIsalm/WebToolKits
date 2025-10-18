7e2526a63fdd79e7bebd5b1853a743616ea0a3b9
Ra_Rafiq <119992536+RofiqulIsalm@users.noreply.github.com>
2025-10-18 20:15:53 +0600

    Updated SIPCalculator.tsx

--- src/pages/SIPCalculator.tsx
+++ src/pages/SIPCalculator.tsx
@@ -697,7 +697,7 @@ const SipCalculator: React.FC = () => {
             <p>r = {r.toFixed(6)}</p>
             <p>(1 + r) = {onePlusR.toFixed(6)}</p>
             <p>(1 + r)^n = {pow.toFixed(6)}</p>
-            <p>Numerator ((1 + r)^n - 1) = {numerator.toFixed(6)}</p>
+            <p>Numerator ((1 + r)<sup>n</sup> - 1) = {numerator.toFixed(6)}</p>
             <p>Denominator (r) = {denominator.toFixed(6)}</p>
             <p>Factor ((1 + r)<sup>n</sup> - 1) / r = {factor.toFixed(6)}</p>
 
