1bbfd155617698df22eb9d11802385804a97d505
Ra_Rafiq <119992536+RofiqulIsalm@users.noreply.github.com>
2025-10-18 20:34:08 +0600

    Updated SIPCalculator.tsx

--- src/pages/SIPCalculator.tsx
+++ src/pages/SIPCalculator.tsx
@@ -738,7 +738,7 @@ const SipCalculator: React.FC = () => {
             <h2 className="text-indigo-400 text-base sm:text-lg font-semibold mb-1 flex items-center gap-2">
               🧮 <span className="text-white/90">Formula:</span>
             </h2>
-            <p className="text-slate-200  text-center font-mono text-[13.5px] sm:text-sm leading-7 break-words">
+            <p className="text-slate-200  text-center font-mono text-[20.5px] sm:text-sm leading-7 break-words">
               <span className="text-indigo-300 font-semibold">FV</span> ={" "}
               <span className="text-white">P</span> × ((1 + <span className="text-emerald-400">r</span>)
               <sup className="text-slate-400">n</sup> − 1) ÷{" "}
