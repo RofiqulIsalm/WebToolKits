557cabf1c73dd7cf985b42bde8f6e92af75d2ea2
Ra_Rafiq <119992536+RofiqulIsalm@users.noreply.github.com>
2025-10-18 10:25:03 +0600

    Updated MortgageCalculator.tsx

--- src/pages/MortgageCalculator.tsx
+++ src/pages/MortgageCalculator.tsx
@@ -929,7 +929,7 @@ const MortgageCalculator: React.FC = () => {
                   <span className="text-white">
                     {formatCurrency(emiSteps.numerator, currentLocale, currency)}
                   </span> 
-                        / ( {emiSteps.pow.toFixed(10)} - 1 ) 
+                        / ( {emiSteps.pow.toFixed(10)} - 1 )  
                      </p>
                  
                  </div>
