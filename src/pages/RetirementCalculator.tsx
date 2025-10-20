814fdc507756040966fe299ea44395240c424ffc
Ra_Rafiq <119992536+RofiqulIsalm@users.noreply.github.com>
2025-10-20 08:51:48 +0600

    Updated RetirementCalculator.tsx

--- src/pages/RetirementCalculator.tsx
+++ src/pages/RetirementCalculator.tsx
@@ -664,7 +664,7 @@ const RetirementCalculator: React.FC = () => {
                                   placeholder="30"
                                   className="w-full bg-[#0f172a] text-white px-4 py-2.5 border border-[#334155] rounded-lg focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500 placeholder-slate-500 text-sm transition-all"
                                 />
-                                <span className="absolute right-4 top-1/2 -translate-y-1/2 text-slate-500 text-xs">
+                                <span className="absolute right-1 top-1/2 -translate-y-1/2 text-slate-500 text-xs">
                                   🎂
                                 </span>
                               </div>
