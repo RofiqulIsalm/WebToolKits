2832c84b7e81f495c7f08ab4f606bbe74cde11f0
Ra_Rafiq <119992536+RofiqulIsalm@users.noreply.github.com>
2025-10-22 15:57:56 +0600

    Updated PrimeNumberChecker.tsx

--- src/pages/PrimeNumberChecker.tsx
+++ src/pages/PrimeNumberChecker.tsx
@@ -56,6 +56,7 @@ const PrimeNumberChecker: React.FC = () => {
           { name: 'Prime Number Checker', url: '/prime-number-checker' }
         ]}
       />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
 
 
       <div className="max-w-4xl mx-auto">
