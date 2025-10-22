8344102f0116002395e7bc2a0437b999e5cde7b0
Ra_Rafiq <119992536+RofiqulIsalm@users.noreply.github.com>
2025-10-22 15:58:15 +0600

    Updated QRCodeGenerator.tsx

--- src/pages/QRCodeGenerator.tsx
+++ src/pages/QRCodeGenerator.tsx
@@ -564,6 +564,8 @@ const generateQRCode = async () => {
             }}
 
           />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      
 
        {/* Performance Preconnect & Preload */}
   <link rel="preconnect" href="https://fonts.googleapis.com" />
