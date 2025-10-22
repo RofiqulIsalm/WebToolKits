2c2d75ec0b9148f8260681ccf99fe22c616cced3
Ra_Rafiq <119992536+RofiqulIsalm@users.noreply.github.com>
2025-10-22 15:31:51 +0600

    Updated SpeedConverter.tsx

--- src/pages/SpeedConverter.tsx
+++ src/pages/SpeedConverter.tsx
@@ -58,6 +58,7 @@ const SpeedConverter: React.FC = () => {
           { name: 'Speed Converter', url: '/speed-converter' }
         ]}
       />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
     <div className="max-w-4xl mx-auto">
       <Breadcrumbs items={[
         { name: 'Unit Converters', url: '/category/unit-converters' },
